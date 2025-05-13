9e66585bd065ee8bb1037d79c6b892af8be26f39
diasoy <22082010134@student.upnjatim.ac.id>
2025-05-13 21:22:57 +0700

    fix: delete build folder

--- public/build/assets/ApplicationLogo-CSuGBocz.js
+++
@@ -1,1 +0,0 @@
-import{j as i}from"./app-AIDFTJRs.js";function r(o){return i.jsx("img",{...o,src:"/medify_logo.svg",alt:"Medify Logo"})}export{r as A};
--- public/build/assets/formatRupiah--QECpPMs.js
+++
@@ -1,1 +0,0 @@
-function a(e){return`Rp ${e.toString().replace(/\D/g,"").replace(/\B(?=(\d{3})+(?!\d))/g,".")}`}export{a as f};
